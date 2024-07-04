0686ca8221e1f9a7c345ef5b6dcf20a954da5e85
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-05 04:40:46 +0800

    Update and rename script/deepcode.js to starling.js

--- starling.js
+++ starling.js
@@ -1,25 +1,25 @@
 const axios = require("axios");
 
 module.exports.config = {
-    name: "deepcode",
+    name: "ai4",
     version: "1.0.0",
-    credits: "chill",
+    credits: "CyyDev",
     description: "Interact with Coder AI",
     hasPrefix: false,
     cooldown: 5,
-    aliases: []
+    aliases: ['starling']
 };
 
 module.exports.run = async function ({ api, event, args }) {
     try {
         let q = args.join(" ");
         if (!q) {
-            return api.sendMessage(" Missing question for the coder", event.threadID, event.messageID);
+            return api.sendMessage("How to use:\n\n• ai4 [query]\n\n• ai4 what are atoms?", event.threadID, event.messageID);
         }
 
-        api.sendMessage("Deepcode, answering please wait...", event.threadID, async (err, info) => {
+        api.sendMessage("Processing query...", event.threadID, async (err, info) => {
             try {
-                const response = await axios.get(`https://joshweb.click/ai/deepseek-coder?q=${encodeURIComponent(q)}&uid=100`);
+                const response = await axios.get(`https://joshweb.click/ai/starling-lm-7b?q=${encodeURIComponent(q)}&uid=100`);
                 const answer = response.data.result;
 
                 api.sendMessage(answer, event.threadID);
