b06f62f7921098035cfb01a2c17684fa7a4e6155
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-06 21:19:46 +0800

    Update 0ai3.js

--- script/0ai3.js
+++ script/0ai3.js
@@ -19,7 +19,7 @@ module.exports.run = async function ({ api, event, args }) {
 
         api.sendMessage("Processing your query...", event.threadID, async (err, info) => {
             try {
-                const response = await axios.get(`https://joshweb.click/ai/zephyr-7b?q={encodeURIComponent(q)}&uid=100`);
+                const response = await axios.get(`https://joshweb.click/ai/zephyr-7b?q=${encodeURIComponent(q)}&uid=100`);
                 const answer = response.data.result;
 
                 api.sendMessage(answer, event.threadID);
@@ -29,7 +29,7 @@ module.exports.run = async function ({ api, event, args }) {
             }
         });
     } catch (error) {
-        console.error("Error in lma command:", error);
+        console.error("Error in ai command:", error);
         api.sendMessage("An error occurred while processing your request.", event.threadID);
     }
 };
