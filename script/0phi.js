51e929fd34f87f8ae8d918b865437226bf47e5e9
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-07 13:30:33 +0800

    Create 0phi.js

---
+++ script/0phi.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+module.exports.config = {
+    name: "phi",
+    version: "1.0.0",
+    credits: "CyDev",
+    description: "Get responses on Microsoft Phi!",
+    hasPrefix: false,
+    cooldown: 5,
+    aliases: ["p", "microsoft"]
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    try {
+        let q = args.join(" ");
+        if (!q) {
+            return api.sendMessage("How to use:\n\n• phi/p [query]\n\n• phi/p who are you?", event.threadID, event.messageID);
+        }
+
+        api.sendMessage("Processing your query...", event.threadID, async (err, info) => {
+            try {
+                const response = await axios.get(`https://joshweb.click/ai/phi-2?q=${encodeURIComponent(q)}&uid=100`);
+                const answer = response.data.result;
+
+                api.sendMessage(answer, event.threadID);
+            } catch (error) {
+                console.error(error);
+                api.sendMessage("An error occurred while processing your request.", event.threadID);
+            }
+        });
+    } catch (error) {
+        console.error("Error in ai command:", error);
+        api.sendMessage("An error occurred while processing your request.", event.threadID);
+    }
+};
