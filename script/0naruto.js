0fd051bcc89598e57426c0bca3df8a69648ac5e9
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-13 18:33:58 +0800

    Create 0naruto.js

---
+++ script/0naruto.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+module.exports.config = {
+    name: "naruto",
+    version: "1.0.0",
+    credits: "Developer",
+    description: "Chat with Naruto (C.AI)",
+    hasPrefix: false,
+    cooldown: 5,
+    aliases: ["uzumaki"]
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    try {
+        let q = args.join(" ");
+        if (!q) {
+            return api.sendMessage("Please provide a query.", event.threadID, event.messageID);
+        }
+
+        api.sendMessage("Processing your query...", event.threadID, async (err, info) => {
+            try {
+                const response = await axios.get(`https://joshweb.click/pai/naruto?q=${encodeURIComponent(q)}&uid=100`);
+                const answer = response.data.result;
+
+                api.sendMessage(answer, event.threadID);
+            } catch (error) {
+                console.error(error);
+                api.sendMessage("An error occurred while processing your request.", event.threadID);
+            }
+        });
+    } catch (error) {
+        console.error("Error in ai command:", error);
+        api.sendMessage("An error occurred while processing your request.", event.threadID);
+    }
+};
