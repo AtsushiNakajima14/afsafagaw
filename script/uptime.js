dff7497cbcec7a88e7997d81882af8961517605c
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-16 19:07:59 +0800

    Update uptime.js

--- script/uptime.js
+++ script/uptime.js
@@ -11,16 +11,21 @@ module.exports.config = {
     aliases: ["upt", "up"]
 };
 
-module.exports.config = async function ({ text, reply }) {
+module.exports['run'] = async function ({ args, api, event }) {
+function reply(m) {
+api.sendMessage(m, event.threadID, event.messageID)
+}
     const time = process.uptime();
     const hours = Math.floor(time / (60 * 60));
     const minutes = Math.floor((time % (60 * 60)) / 60);
     const seconds = Math.floor(time % 60);
-    const id = text[0] || "4";
-
-    const res = (await axios.get(global.deku.ENDPOINT + `/canvas/uptime?id=${id}&instag=cyyydev&ghub=Unknown&fb=Cyy%20Dev&hours=${hours}&minutes=${minutes}&seconds=${seconds}&botname=SemBlance%20AutoBot`, {
+    const id = args[0] || "4";
+    const bot = "SemBlance Autobot";
+    const insta = "cyyydev";
+    const gh = "Unknown";
+    const fb = "Cy Dev";
+    const res = (await axios.get('https://joshweb.click' + `/canvas/uptime?id=${id}&instag=${insta}&ghub=${gh}&fb=${fb}&hours=${hours}&minutes=${minutes}&seconds=${seconds}&botname=${bot}`, {
         responseType: "stream"
     })).data;
-    
     return reply({body: "⌛:" + hours + ":" + minutes + ":" + seconds + "\nCharacter ID: " + id, attachment: res});
 };
