95c185ac22788605499978686f0dd25c3415b6a7
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-17 18:44:57 +0800

    Update imgur.js

--- script/imgur.js
+++ script/imgur.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	name: "imgur",
 	version: "1.0.0",
 	role: 0,
-	hasPrefix: false,
+	hasPrefix: true,
 	credits: "Eugene Aguilar",
 	description: "upload to imgur",
 	usages: "imgur reply image,video,png,jpg",
