dc44def73f4dd1e8ec5b5419f3b2491130270c36
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-17 18:35:12 +0800

    Update 0boruto.js

--- script/0boruto.js
+++ script/0boruto.js
@@ -5,7 +5,7 @@ module.exports.config = {
     version: "1.0.0",
     credits: "Developer",
     description: "Chat with Boruto (C.AI)",
-    hasPrefix: false,
+    hasPrefix: true,
     cooldown: 5,
     aliases: ["bor"]
 };
