2169d0108b69359dba68d18c19b64b479a7f7639
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-17 18:36:26 +0800

    Update 0gojo.js

--- script/0gojo.js
+++ script/0gojo.js
@@ -5,7 +5,7 @@ module.exports.config = {
     version: "1.0.0",
     credits: "Developer",
     description: "Chat with Gojo (C.AI)",
-    hasPrefix: false,
+    hasPrefix: true,
     cooldown: 5,
     aliases: ["satoru"]
 };
