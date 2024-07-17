0486b46d13da2164ac80c9cce2bd4984ae3488da
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-17 18:41:34 +0800

    Update egif.js

--- script/egif.js
+++ script/egif.js
@@ -7,7 +7,7 @@ module.exports.config = {
     version: "1.0.0",
     credits: "chill",
     description: "Convert an emoji to a GIF ",
-    hasPrefix: false,
+    hasPrefix: true,
     cooldown: 5,
     aliases: ["egif"]
 };
