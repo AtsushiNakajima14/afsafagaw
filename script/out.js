ea9156cc61dc3b0b03531a98bee2de0895b32a38
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-07-17 18:47:10 +0800

    Update out.js

--- script/out.js
+++ script/out.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "out",
 	version: "1.0.0",
 	role: 2,
-	hasPrefix: false,
+	hasPrefix: true,
 	credits: "Developer",
 	description: "Bot leaves the thread",
 	usages: "out",
