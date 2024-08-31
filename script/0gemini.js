f731eea84111f1a337626a7eda16c201a8dc9de2
AtsushiNakajima14 <140885940+AtsushiNakajima14@users.noreply.github.com>
2024-08-31 12:36:29 +0800

    Update 0gemini.js

--- script/0gemini.js
+++ script/0gemini.js
@@ -1,37 +1,39 @@
 const axios = require('axios');
 
 module.exports.config = {
-  name: "gemini",
-  role: 0,
-  credits: "Developer",
-  description: "Interact with Gemini API",
-  hasPrefix: true,
-  version: "1.0.0",
-  aliases: ["bard", "gemini"],
-  usage: "gemini [prompt]"
+    name: "analyser",
+    role: 0,
+    credits: "Developer",
+    description: "Interact with Gemini",
+    hasPrefix: true,
+    version: "1.0.0",
+    aliases: ["imageai"],
+    usage: "gemini [reply to photo]"
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const prompt = args.join(" ");
+    const prompt = args.join(" ");
 
-  if (!prompt) {
-    return api.sendMessage('Please provide a query..', event.threadID, event.messageID);
-  }
+    if (!prompt) {
+        return api.sendMessage('This command is only for images.', event.threadID, event.messageID);
+    }
 
-  if (event.type !== "message_reply" || !event.messageReply.attachments[0] || event.messageReply.attachments[0].type !== "photo") {
-    return api.sendMessage('Please reply to a photo with this command.', event.threadID, event.messageID);
-  }
+    if (event.type !== "message_reply" || !event.messageReply.attachments[0] || event.messageReply.attachments[0].type !== "photo") {
+        return api.sendMessage('Please reply to a photo with this command.', event.threadID, event.messageID);
+    }
 
-  const url = encodeURIComponent(event.messageReply.attachments[0].url);
-  api.sendTypingIndicator(event.threadID);
+    const url = encodeURIComponent(event.messageReply.attachments[0].url);
+    api.sendTypingIndicator(event.threadID);
 
-  try {
-    const response = await axios.get(`https://ggwp-yyxy.onrender.com/gemini?prompt=${encodeURIComponent(prompt)}&url=${url}`);
-    const description = response.data.gemini;
+    try {
+        await api.sendMessage('\n━━━━━━━━━━━━━━━━━━\nrecognizing picture, please wait...\n━━━━━━━━━━━━━━━━━━', event.threadID);
 
-    return api.sendMessage(description, event.threadID, event.messageID);
-  } catch (error) {
-    console.error(error);
-    return api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
-  }
+        const response = await axios.get(`https://deku-rest-api.gleeze.com/gemini?prompt=${encodeURIComponent(prompt)}&url=${url}`);
+        const description = response.data.gemini;
+
+        return api.sendMessage(`IMAGE ANALYZER AI\n━━━━━━━━━━━━━━━━━━\n${description}\n━━━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);
+    } catch (error) {
+        console.error(error);
+        return api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
+    }
 };
